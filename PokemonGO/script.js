94ab3225208a0d66b90656c87da357bcaeb5342a
Jericho <165892976+Antarticstorm@users.noreply.github.com>
2025-07-05 16:02:38 +0900

    added up to the top buitton removed header fixdd the color palette a bit

--- PokemonGO/script.js
+++ PokemonGO/script.js
@@ -28,6 +28,25 @@ document.addEventListener("click", (event) => {
   }
 });
 
+// Go to top button
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 
 // Slideshow
